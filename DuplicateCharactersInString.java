90195e018d0be4dcac9980e46f736a8d9e7cb5ee
PriyaBiradarDev <49949656+PriyaBiradarDev@users.noreply.github.com>
2019-10-11 22:17:02 +0530

    Add files via upload
    using collection

---
+++ DuplicateCharactersInString.java
@@ -0,0 +1,40 @@
+package com.string.nt;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class DuplicateCharactersInString {
+	public static void main(String[] args) {
+		String s1 = "PriyaPiyu";
+		findDuplicateChar(s1);
+	}
+
+	static void findDuplicateChar(String s1) {
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+		char[] ch = s1.toCharArray();
+		for (char c : ch) {
+			if (hm.containsKey(c)) {
+				hm.put(c, hm.get(c) + 1);
+
+			} else {
+				hm.put(c, 1);
+			}
+
+		}
+		// Getting a Set containing all keys of hm
+		
+		Set<Character> charsInString = hm.keySet();
+
+		System.out.println("Duplicate Characters In " + s1);
+
+		// Iterating through Set 'charsInString'
+		for (char cc : charsInString) {
+			if (hm.get(cc) > 1) {
+				// If any char has a count of more than 1, printing it's count
+
+				System.out.println(cc + " : " + hm.get(cc));
+			}
+		}
+
+	}
+}
